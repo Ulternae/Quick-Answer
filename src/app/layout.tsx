5c47eb557bf4f0f6f269d7b1816e265fcd83f82c
Ulternae <ulternae@gmail.com>
2026-07-24 13:39:38 -0500

    chore: configure shadcn/ui styles and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { Kameron } from "next/font/google";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const kameron = Kameron({
+  variable: "--font-kameron",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -23,10 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={cn("h-full antialiased dark", kameron.className)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       asdasd
+      <Button>asd</Button>
     </div>
   );
 }
